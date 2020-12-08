53021dcc42b32dc250252050d053fecfb6f4d479
Boris Timofeenko <timbori@mail.ru>
2020-12-08 22:06:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        printf("Hello World... from thread = %d\n",
+               omp_get_thread_num());
+    }
+    return 0;
+}
